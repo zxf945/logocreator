89e7c01681f81aa2ea34771882aededf9032baab
Sam Selikoff <sam.selikoff@gmail.com>
2024-11-12 15:14:47 -0500

    wip

--- app/api/generate-logo/route.ts
+++ app/api/generate-logo/route.ts
@@ -13,7 +13,7 @@ if (process.env.UPSTASH_REDIS_REST_URL) {
   ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     // Allow 100 requests per day (~5-10 prompts)
-    limiter: Ratelimit.fixedWindow(2, "1440 m"),
+    limiter: Ratelimit.fixedWindow(100, "1440 m"),
     analytics: true,
     prefix: "logocreator",
   });
