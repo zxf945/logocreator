c6ba0906cfb02e827584e8d0563e11049ff24587
Sam Selikoff <sam.selikoff@gmail.com>
2024-11-12 17:04:28 -0500

    Helicone

--- app/api/generate-logo/route.ts
+++ app/api/generate-logo/route.ts
@@ -33,16 +33,16 @@ export async function POST(req: Request) {
     .parse(json);
 
   // Add observability if a Helicone key is specified, otherwise skip
-  // const options: ConstructorParameters<typeof Together>[0] = {};
-  // if (process.env.HELICONE_API_KEY) {
-  //   options.baseURL = "https://together.helicone.ai/v1";
-  //   options.defaultHeaders = {
-  //     "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
-  //     "Helicone-Property-BYOK": userAPIKey ? "true" : "false",
-  //   };
-  // }
+  const options: ConstructorParameters<typeof Together>[0] = {};
+  if (process.env.HELICONE_API_KEY) {
+    options.baseURL = "https://together.helicone.ai/v1";
+    options.defaultHeaders = {
+      "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
+      "Helicone-Property-BYOK": data.userAPIKey ? "true" : "false",
+    };
+  }
 
-  const client = new Together();
+  const client = new Together(options);
 
   if (data.userAPIKey) {
     client.apiKey = data.userAPIKey;
